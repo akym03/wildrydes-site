7ba7c37bbc2836b051e9ec2791b03816dfd951d4
masahiro akiyama <akiyama@bk.iij4u.or.jp>
2023-03-28 21:56:59 +0900

    setup cognito userpool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_vyq4Vx4Tj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '83ipfnh3v0e1gobjke0koo9li', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
